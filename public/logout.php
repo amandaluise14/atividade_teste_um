fd9316e2e0ae6b15794fda4891b3a003ae834ec4
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-06-01 09:16:08 -0300

    doc - adicionei comentários explicando as funções do código no arquivo logout.php.

--- public/logout.php
+++ public/logout.php
@@ -1,6 +1,9 @@
 <?php
+ // Inicia a sessão atual para que ela possa ser manipulada
     session_start();
+// Remove todos os dados na sessão
     session_destroy();
+// Redireciona para a página inicial
     header("Location: ../index.php");
     exit();
 ?>
